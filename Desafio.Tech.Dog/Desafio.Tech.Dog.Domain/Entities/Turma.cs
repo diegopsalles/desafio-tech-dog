d2fe9055f04592a8ab9ecbc46002e708288a6637
Diego Pereira Salles <diego.salles@wooza.com.br>
2020-03-30 18:49:26 -0300

    implementação da camada de dominio

---
+++ Desafio.Tech.Dog/Desafio.Tech.Dog.Domain/Entities/Turma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio.Tech.Dog.Domain.Entities
+{
+    public class Turma
+    {
+        public int IdTurma { get; set; }
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+
+        public List<Aluno> Alunos { get; set; }
+    }
+}
